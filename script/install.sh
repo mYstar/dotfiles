ba238604b42e673c67687f132209561ca8d3c011
mYstar <eric_st@web.de>
2016-11-03 16:13:27 +0100

    reworked installation process

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,13 @@
-# install the TU Dresden Cloud (prerequisite: install ownCloud and login)
-mkdir ~/cloud
-encfs ~/ownCloud/encrypted ~/cloud
+sudo ~/.dotfiles/script/install_sudo.sh
 
-# create the link to the scripts in homedir
-ln --symbolic ~/.dotfiles/script ~
+## installation
+~/.dotfiles/script/bootstrap
+
+pip install --upgrade neovim
+pip3 install --upgrade neovim
+
+mkdir ~/.ssh
+ssh-keygen -t rsa -b 4096 -C "eric_st@web.de"
+ssh-add ~/.ssh/id_rsa
+
+chsh -s /usr/bin/zsh
---
+++ script/install_sudo.sh
@@ -0,0 +1,18 @@
+## cloud
+apt-get install keepassx owncloud-client
+
+## programming
+apt-get install python3 python-dev python-pip python3-dev python3-pip git
+
+## console
+sudo apt-get install software-properties-common
+apt-get install vim zsh
+add-apt-repository ppa:neovim-ppa/unstable
+apt-get update
+apt-get install neovim
+
+## office
+apt-get install libreoffice texlive
+
+## media
+apt-get install vlc
